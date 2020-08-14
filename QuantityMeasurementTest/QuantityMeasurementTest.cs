c4961627f1691fb2e01618e3eb7c222da5f0666c
gajanan15 <gajanandesai97@gmail.com>
2020-08-14 16:44:47 +0530

    [Gajanan]ADDED : Test For Thirty Six Inch And One Yard Is Equal

--- QuantityMeasurementTest/QuantityMeasurementTest.cs
+++ QuantityMeasurementTest/QuantityMeasurementTest.cs
@@ -277,5 +277,16 @@ namespace QuantityMeasurementTest
             this.inchTwo = this.quantityMeasurement.ReturnUnits(QuantityMeasurement.LengthTypeEnum.LengthType.INCHES, 36.0);
             Assert.AreEqual(this.inchOne, this.inchTwo);
         }
+
+        /// <summary>
+        /// Test Case 2.6 To Check Given Thirty Six Inch is equal To One Yard.
+        /// </summary>
+         [Test]
+         public void GivenThirtySixInchAndOneYard_WhenEqual_ShouldReturnTrue()
+        {
+            this.inchOne = this.quantityMeasurement.ReturnUnits(QuantityMeasurement.LengthTypeEnum.LengthType.INCHES, 36.0);
+            this.inchTwo = this.quantityMeasurement.ReturnUnits(QuantityMeasurement.LengthTypeEnum.LengthType.YARD, 1.0);
+            Assert.AreEqual(this.inchOne, this.inchTwo);
+        }
     }
 }
